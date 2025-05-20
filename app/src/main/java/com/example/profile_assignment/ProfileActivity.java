8ea54ea313a2d4004a28f08c1aca2e69b908c5d2
priyanshu-shinde26 <priyanshu1983s@gmail.com>
2025-05-20 19:00:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/profile_assignment/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.profile_assignment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_assignment);
+
+        // You can add any additional functionality here
+    }
+}
